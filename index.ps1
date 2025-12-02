184d26ce57f5b0cb298f1d70a55df03f865cf77f
JOJOTTO <115787204+JOJOTTO@users.noreply.github.com>
2025-12-02 13:06:08 +0100

    Delete index.ps1

--- index.ps1
+++
@@ -1,9 +0,0 @@
-# Import common_functions early so logging helpers are available
-$ModulePath = ".\common_functions\common_functions.psm1"
-try {
-    Import-Module $ModulePath -ErrorAction Stop
-} catch {
-    Write-Warning "Le module common_functions n'a pas pu être importé: $_"
-    exit 1
-}
-
