2c5d1df299383cbcafc13542331671aef2dd4392
Raishyn <casagrande.michael@outlook.fr>
2025-12-12 17:35:20 +0100

    Add files via upload

---
+++ Modules/AuditCore/Public/Get-JEAAudit.ps1
@@ -0,0 +1,52 @@
+﻿function Get-JEAAudit {
+    [CmdletBinding()]
+    param()
+
+    $result = [pscustomobject]@{
+        WinRmState           = $null
+        HasJEASessionConfig  = $false
+        SessionConfigurations= @()
+        RoleCapabilityFiles  = @()
+        PSModulePaths        = $null
+    }
+
+    $WinRM = Get-service winrm
+
+    if($WinRM.Status -eq "Running"){
+
+        $sessionConfigs = Get-PSSessionConfiguration -ErrorAction SilentlyContinue | Select-Object Name, Permission
+
+
+        if ($sessionConfigs) {
+            $result.HasJEASessionConfig   = $true
+            $result.SessionConfigurations = $sessionConfigs
+        }
+
+        $psModulePath = $env:PSModulePath -split ';'
+
+        $result.PSModulePaths = $psModulePath
+
+        $files = @()
+        foreach ($p in $psModulePath) {
+                if (Test-Path $p) {
+                    $files += Get-ChildItem -Path $p -Recurse -Filter *.psrc -ErrorAction SilentlyContinue
+                }
+        }
+
+        if ($files) {
+            $result.RoleCapabilityFiles = $files.Name
+        }
+    
+    }else{
+        
+        $result.WinRmState = $WinRM.Status
+        $result.HasJEASessionConfig = "N/A"
+        $result.SessionConfigurations = "N/A" 
+        $result.RoleCapabilityFiles = "N/A"
+        $result.PSModulePaths = "N/A"
+
+    }
+
+    Return $result
+
+}
---
+++ Modules/AuditCore/Public/Get-UACAudit.ps1
@@ -0,0 +1,64 @@
+﻿function Get-UACAudit {
+    [CmdletBinding()]
+    param()
+
+    $result = [pscustomobject]@{
+        EnableLUA                 = $null   # UAC global on/off
+        ConsentPromptAdmin        = $null   # ConsentPromptBehaviorAdmin
+        ConsentPromptUser         = $null   # ConsentPromptBehaviorUser
+        PromptOnSecureDesktop     = $null   # PromptOnSecureDesktop
+        FilterAdministratorToken  = $null   # Reco: 1
+        LocalAccountTokenFilter   = $null   # Reco: 1
+        EnableUIADesktopToggle    = $null
+        EnableInstallerDetection  = $null
+        ValidateAdminCodeSignatures = $null
+        EnableSecureUIAPaths = $null
+        EnableVirtualization = $null
+        InteractiveLogonFirst = $null
+    }
+
+    # Clé principale UAC
+    $uacReg = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -ErrorAction SilentlyContinue
+
+    if ($uacReg) {
+        $result.EnableLUA             = $uacReg.EnableLUA
+        $result.ConsentPromptAdmin    = $uacReg.ConsentPromptBehaviorAdmin
+        $result.ConsentPromptUser     = $uacReg.ConsentPromptBehaviorUser
+        $result.PromptOnSecureDesktop = $uacReg.PromptOnSecureDesktop
+        $result.FilterAdministratorToken = $uacReg.FilterAdministratorToken
+        $result.EnableUIADesktopToggle = $uacReg.EnableUIADesktopToggle
+        $result.EnableInstallerDetection = $uacReg.EnableInstallerDetection
+        $result.ValidateAdminCodeSignatures = $uacReg.ValidateAdminCodeSignatures
+        $result.EnableSecureUIAPaths = $uacReg.EnableSecureUIAPaths
+        $result.EnableVirtualization = $uacReg.EnableVirtualization
+    }
+
+    switch($uacReg.LocalAccountTokenFilterPolicy){
+
+        1{$result.LocalAccountTokenFilter = "Enabled"}
+        0{$result.LocalAccountTokenFilter = "Disabled"}
+        $null{$result.LocalAccountTokenFilter = "Key not found"}
+        Default{$result.LocalAccountTokenFilter = "Unknown Value"}
+
+    }
+
+    switch($uacReg.FilterAdministratorToken){
+        
+        1{$result.FilterAdministratorToken = "Enabled"}
+        0{$result.FilterAdministratorToken = "Disabled"}
+        $null{$result.FilterAdministratorToken = "Key not found"}
+        Default{$result.FilterAdministratorToken = "Unknown Value"}
+
+    }
+
+    switch($uacReg.InteractiveLogonFirst){
+
+        1{$result.InteractiveLogonFirst = "Enabled"}
+        0{$result.InteractiveLogonFirst = "Disabled"}
+        $null{$result.InteractiveLogonFirst = "Key not found"}
+        Default{$result.InteractiveLogonFirst = "Unknown Value"}
+
+    }
+
+    Return $result
+}
