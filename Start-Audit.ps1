3cf4bd14503de438693c6fd70fd61ec034d2cf64
JOJOTTO <115787204+JOJOTTO@users.noreply.github.com>
2025-12-17 22:32:45 +0100

    Add files via upload

--- Start-Audit.ps1
+++ Start-Audit.ps1
@@ -13,40 +13,163 @@ else {
     exit
 }
 
-# --- ExÈcution de l'Audit ---
-Write-Host "DÈmarrage de l'audit sur $env:COMPUTERNAME..." -ForegroundColor Green
+# --- Ex√©cution de l'Audit ---
+Write-Host "D√©marrage de l'audit sur $env:COMPUTERNAME..." -ForegroundColor Green
 
-# 1. RÈcupÈration du contexte (appel de notre fonction importÈe)
+# 1. R√©cup√©ration du contexte (appel de notre fonction import√©e)
 $context = Get-HostContext
 
 if ($context) {
-    Write-Host "`n[+] Contexte IdentifiÈ :" -ForegroundColor Yellow
+    Write-Host "`n[+] Contexte Identifi√© :" -ForegroundColor Yellow
     $context | Format-List
     
-    # Logique conditionnelle basÈ sur le contexte
+    # Logique conditionnelle bas√© sur le contexte
     if ($context.OSRole -eq "Server") {
-        Write-Host ">> Mode Audit Serveur activÈ." -ForegroundColor Gray
+        Write-Host ">> Mode Audit Serveur activ√©." -ForegroundColor Gray
     }
     elseif ($context.HardwareType -eq "Virtual Machine") {
-        Write-Host ">> Machine Virtuelle dÈtectÈe : VÈrification des Integration Tools requise." -ForegroundColor Gray
+        Write-Host ">> Machine Virtuelle d√©tect√©e : V√©rification des Integration Tools requise." -ForegroundColor Gray
     }
 }
 else {
-    Write-Error "Impossible de dÈterminer le contexte de la machine."
+    Write-Error "Impossible de d√©terminer le contexte de la machine."
 }
 
+########## Local User Audit ##########
 
 $localUserAudit = Get-LocalUserAudit
 
 if ($localUserAudit) {
-    Write-Host "`n[+] Compte locaux identifiÈs :" -ForegroundColor Yellow
+    Write-Host "`n[+] Compte locaux identifi√©s :" -ForegroundColor Gray
     
-    if (($localUserAudit.AdminAccountSID -match "-500$") -and ($localUserAudit.AdminEnabled -eq $false)){
-        Write-Host "`n Le compte Administrateur par dÈfaut est activÈ" -ForegroundColor Red
+    if (($localUserAudit.AdminAccountSID -match "-500$") -and ($localUserAudit.AdminEnabled -eq $true)){
+        Write-Host "`nLe compte Administrateur par d√©faut est activ√©" -ForegroundColor Red
         Write-Host $localUserAudit.AdminRecommandation.Enabled -ForegroundColor Yellow     
+    }
+    else {
+        Write-Host $localUserAudit.AdminRecommandation.Disabled -ForegroundColor Green
+    }
+
+    if (($localUserAudit.GuestAccountSID -match "-501$") -and ($localUserAudit.GuestEnabled -eq $true)){
+        Write-Host "`nLe compte Invit√© par d√©faut est activ√©" -ForegroundColor Red
+        Write-Host $localUserAudit.GuestRecommandation.Enabled -ForegroundColor Yellow
+    } else {
+        Write-Host $localUserAudit.GuestRecommandation.Disabled -ForegroundColor Green
+    }
+}
+else {
+    Write-Error "Impossible d'auditer les utilisateurs locaux"
+}
+
+########## LAPS Audit ##########
+
+$lapsAudit = Get-LAPSAudit
+Write-Host "`n[+] Audit de la configuration LAPS :" -Foregroundcolor Gray
+
+if ($lapsaudit){
+
+    if ($context.Domainjoined -eq $true) {
+
+        # Affichage dynamique selon le r√©sultat
+        switch ($lapsAudit.Status) {
+            "PASS" {
+                Write-Host "   [OK] $($lapsAudit.DetectedMethods)" -ForegroundColor Green
+                # Si on a un warning mineur (ex: Legacy + Modern en m√™me temps)
+                if ($lapsAudit.Recommendation -match "ATTENTION") {
+                    Write-Host "   $($lapsAudit.Recommendation)" -ForegroundColor Magenta
+                }
+            }
+            "WARNING" {
+                # Cas sp√©cifique Legacy seul
+                Write-Host "   [OBSOLETE] $($lapsAudit.DetectedMethods)" -ForegroundColor Orange
+                Write-Host "   -> $($lapsAudit.Recommendation)" -ForegroundColor Yellow
+            }
+            "FAIL" {
+                Write-Host "   [ALERTE] $($lapsAudit.Recommendation)" -ForegroundColor Red
+            }
+        }
+
+    }
+    else {
+        Write-Host "`rLa machine n'est pas jointe √† un domaine. LAPS n'est pas auditable" -ForegroundColor Magenta
+    }
+}
+
+########## Active Directory Password Policy Audit ##########
+
+if ($context.Domainjoined -eq $true){
+    $adPasswordPolicy = Get-ADPolPassAudit
+
+    if ($adPasswordPolicy -and $context.Domainjoined -eq $true) {
+
+        Write-Host "`n[+] Audit des politiques de mots de passe Active Directory :" -Foregroundcolor Gray
+        try {
+            # Affichage Longueur
+            if ($adPasswordPolicy.MinLengthStatus -eq "FAIL") {
+                Write-Host "   [LONGUEUR]   [ALERTE] $($adPasswordPolicy.MinLengthReco)" -ForegroundColor Red
+            } else {
+                Write-Host "   [LONGUEUR]   [OK] $($adPasswordPolicy.MinLengthReco)" -ForegroundColor Green
+            }
+
+            # Affichage Complexit√©
+            if ($adPasswordPolicy.ComplexityStatus -eq "FAIL") {
+                Write-Host "   [COMPLEXITE] [ALERTE] $($adPasswordPolicy.ComplexityReco)" -ForegroundColor Red
+            } else {
+                Write-Host "   [COMPLEXITE] [OK] $($adPasswordPolicy.ComplexityReco)" -ForegroundColor Green
+            }
+
+            # Affichage Verrouillage
+            if ($adPasswordPolicy.LockoutStatus -eq "FAIL") {
+                Write-Host "   [BLOCAGE]    [ALERTE] $($adPasswordPolicy.LockoutReco)" -ForegroundColor Red
+            } elseif ($adPasswordPolicy.LockoutStatus -eq "WARNING") {
+                Write-Host "   [BLOCAGE]    [MOYEN] $($adPasswordPolicy.LockoutReco)" -ForegroundColor Magenta
+            } else {
+                Write-Host "   [BLOCAGE]    [OK] $($adPasswordPolicy.LockoutReco)" -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Host "Erreur lors de l'affichage des r√©sultats de l'audit de la politique de mot de passe AD"
+        }
+    }
+} else {
+########## Local Password Policy Audit ##########
+
+    Write-Host "`n[+] Audit des politiques de mots de passe :" -Foregroundcolor Gray
+    $passwordPolicy = Get-PolPassAudit
+
+    if ($passwordPolicy){
+        try {
+            # Affichage Longueur
+            if ($passwordPolicy.MinLengthStatus -eq "FAIL") {
+                Write-Host "   [LONGUEUR]   [ALERTE] $($passwordPolicy.MinLengthReco)" -ForegroundColor Red
+            } else {
+                Write-Host "   [LONGUEUR]   [OK] $($passwordPolicy.MinLengthReco)" -ForegroundColor Green
+            }
+
+            # Affichage Complexit√©
+            if ($passwordPolicy.ComplexityStatus -eq "FAIL") {
+                Write-Host "   [COMPLEXITE] [ALERTE] $($passwordPolicy.ComplexityReco)" -ForegroundColor Red
+            } else {
+                Write-Host "   [COMPLEXITE] [OK] $($passwordPolicy.ComplexityReco)" -ForegroundColor Green
+            }
+
+            # Affichage Verrouillage
+            if ($passwordPolicy.LockoutStatus -eq "FAIL") {
+                Write-Host "   [BLOCAGE]    [ALERTE] $($passwordPolicy.LockoutReco)" -ForegroundColor Red
+            } elseif ($passwordPolicy.LockoutStatus -eq "WARNING") {
+                Write-Host "   [BLOCAGE]    [MOYEN] $($passwordPolicy.LockoutReco)" -ForegroundColor Magenta
+            } else {
+                Write-Host "   [BLOCAGE]    [OK] $($passwordPolicy.LockoutReco)" -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Host "Erreur lors de l'affichage des r√©sultats de l'audit de la politique de mot de passe"
+        }
     }
 }
 
 
+# Start-Sleep -Seconds 30
 # --- Fin ---
-Write-Host "`nAudit terminÈ." -ForegroundColor Cyan
+Write-Host "`nAudit termin√©." -ForegroundColor Cyan
+
